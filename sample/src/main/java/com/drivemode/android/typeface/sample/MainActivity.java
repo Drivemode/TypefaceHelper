b1f64900c0b488348aff5e81f5b9ce0f667210fa
KeithYokoma <Keith.Yokoma@gmail.com>
2014-09-09 17:48:13 +0900

    Add list view sample

--- sample/src/main/java/com/drivemode/android/typeface/sample/MainActivity.java
+++ sample/src/main/java/com/drivemode/android/typeface/sample/MainActivity.java
@@ -3,6 +3,11 @@ package com.drivemode.android.typeface.sample;
 import android.os.Bundle;
 import android.support.v7.app.ActionBarActivity;
 import android.view.Menu;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+import android.widget.TextView;
 
 import com.drivemode.android.typeface.TypefaceHelper;
 
@@ -14,7 +19,17 @@ public class MainActivity extends ActionBarActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-		TypefaceHelper.getInstance().setTypeface(this, "Isserley-Regular.ttf");
+        TypefaceHelper.getInstance().setTypeface(this, "Isserley-Regular.ttf");
+        ListView lv = (ListView) findViewById(R.id.list_view);
+        lv.setAdapter(new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1,
+                android.R.id.text1, getResources().getStringArray(R.array.sample_list)) {
+            @Override
+            public View getView(int position, View convertView, ViewGroup parent) {
+                View view = super.getView(position, convertView, parent);
+                TypefaceHelper.getInstance().setTypeface((TextView) view, "Isserley-Regular.ttf");
+                return view;
+            }
+        });
     }
 
     @Override
