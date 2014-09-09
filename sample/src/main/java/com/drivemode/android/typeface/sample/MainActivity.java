9c1007e88a06183d18b19bc368b05f2bf87e67a4
KeithYokoma <Keith.Yokoma@gmail.com>
2014-09-09 18:00:45 +0900

    Add actionbar tab example

--- sample/src/main/java/com/drivemode/android/typeface/sample/MainActivity.java
+++ sample/src/main/java/com/drivemode/android/typeface/sample/MainActivity.java
@@ -1,6 +1,8 @@
 package com.drivemode.android.typeface.sample;
 
 import android.os.Bundle;
+import android.support.v4.app.FragmentTransaction;
+import android.support.v7.app.ActionBar;
 import android.support.v7.app.ActionBarActivity;
 import android.view.Menu;
 import android.view.View;
@@ -14,12 +16,17 @@ import com.drivemode.android.typeface.TypefaceHelper;
 /**
  * @author KeithYokoma
  */
-public class MainActivity extends ActionBarActivity {
+public class MainActivity extends ActionBarActivity implements ActionBar.TabListener {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        TypefaceHelper.getInstance().setTypeface(this, "Isserley-Regular.ttf");
+
+        ActionBar actionBar = getSupportActionBar();
+        actionBar.setNavigationMode(ActionBar.NAVIGATION_MODE_TABS);
+        actionBar.addTab(actionBar.newTab().setText("Hoge").setTabListener(this));
+        actionBar.addTab(actionBar.newTab().setText("Fuga").setTabListener(this));
+
         ListView lv = (ListView) findViewById(R.id.list_view);
         lv.setAdapter(new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1,
                 android.R.id.text1, getResources().getStringArray(R.array.sample_list)) {
@@ -30,6 +37,8 @@ public class MainActivity extends ActionBarActivity {
                 return view;
             }
         });
+
+        TypefaceHelper.getInstance().setTypeface(this, "Isserley-Regular.ttf");
     }
 
     @Override
@@ -37,4 +46,13 @@ public class MainActivity extends ActionBarActivity {
         getMenuInflater().inflate(R.menu.main, menu);
         return true;
     }
+
+    @Override
+    public void onTabSelected(ActionBar.Tab tab, FragmentTransaction fragmentTransaction) {}
+
+    @Override
+    public void onTabUnselected(ActionBar.Tab tab, FragmentTransaction fragmentTransaction) {}
+
+    @Override
+    public void onTabReselected(ActionBar.Tab tab, FragmentTransaction fragmentTransaction) {}
 }
